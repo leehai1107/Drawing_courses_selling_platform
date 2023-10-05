835c849a90df1a69ea8a8a84b1d8770fd8916889
leehai1107 <leehai1107@gmail.com>
2023-10-06 06:16:45 +0700

    update entity User and Role

--- src/main/java/com/main/drawingcourse/entity/Role.java
+++ src/main/java/com/main/drawingcourse/entity/Role.java
@@ -1,12 +1,13 @@
 package com.main.drawingcourse.entity;
 
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,7 +20,6 @@ public class Role {
 	@Column
 	private String roleName;
 	
-	@ManyToOne
-	@JoinColumn(name = "userId")
-	private User user;
+	@OneToMany(mappedBy = "role")
+	private Collection<User> users;
 }
--- src/main/java/com/main/drawingcourse/entity/User.java
+++ src/main/java/com/main/drawingcourse/entity/User.java
@@ -10,6 +10,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -49,8 +51,9 @@ public class User {
 	@Column(unique = true)
 	private String email;
 	
-	@OneToMany(mappedBy = "user")
-	private Collection<Role> roles;
+	@ManyToOne
+	@JoinColumn(name = "roleId")
+	private Role role;
 	
 	@OneToMany(mappedBy = "user")
 	private Collection<Course> course;
