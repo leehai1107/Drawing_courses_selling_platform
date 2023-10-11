2ddaf5e0b7af6c6522441ba5dfbb5f3c4dbf814f
Le Hai <108707183+leehai1107@users.noreply.github.com>
2023-10-11 11:17:16 +0700

    Update LevelController.java

--- src/main/java/com/main/drawingcourse/controller/LevelController.java
+++ src/main/java/com/main/drawingcourse/controller/LevelController.java
@@ -4,22 +4,12 @@ import com.main.drawingcourse.dto.LevelModel;
 import com.main.drawingcourse.repository.LevelRepository;
 import com.main.drawingcourse.service.ILevelService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("level")
-=======
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("public/level")
->>>>>>> update/main
 public class LevelController {
     @Autowired
     LevelRepository levelRepository;
@@ -31,12 +21,8 @@ public class LevelController {
     public LevelModel AddLevel(@RequestBody LevelModel levelModel){
         return levelService.AddLevel(levelModel);
     }
-<<<<<<< HEAD
-
-=======
 @GetMapping("/ViewAllLevels")
     public List<LevelModel> findAll() {
         return levelService.findAll();
     }
->>>>>>> update/main
 }
