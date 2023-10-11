77c91a5b3a92a8acfc9441858bc8ea68deb16872
Le Hai <108707183+leehai1107@users.noreply.github.com>
2023-10-11 11:21:46 +0700

    Update IDrawingCategoryService.java

--- src/main/java/com/main/drawingcourse/service/IDrawingCategoryService.java
+++ src/main/java/com/main/drawingcourse/service/IDrawingCategoryService.java
@@ -3,15 +3,10 @@ package com.main.drawingcourse.service;
 import com.main.drawingcourse.dto.DrawingCategoryModel;
 import com.main.drawingcourse.entity.DrawingCategory;
 
-<<<<<<< HEAD
-public interface IDrawingCategoryService {
-    DrawingCategoryModel AddCategory(DrawingCategoryModel categoryModel);
-=======
 import java.util.List;
 
 public interface IDrawingCategoryService {
     DrawingCategoryModel AddCategory(DrawingCategoryModel categoryModel);
 
     List<DrawingCategoryModel> findAll();
->>>>>>> update/main
 }
