43a000cb524351a9e387968a71b65527a8bcab7d
Le Hai <108707183+leehai1107@users.noreply.github.com>
2023-10-11 11:14:53 +0700

    Update CourseController.java

--- src/main/java/com/main/drawingcourse/controller/CourseController.java
+++ src/main/java/com/main/drawingcourse/controller/CourseController.java
@@ -3,22 +3,12 @@ package com.main.drawingcourse.controller;
 import com.main.drawingcourse.dto.CourseModel;
 import com.main.drawingcourse.service.ICourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("course")
-=======
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("public/course")
->>>>>>> update/main
 public class CourseController {
     @Autowired
     ICourseService CourseService;
@@ -40,4 +30,3 @@ public class CourseController {
         return CourseService.findAll();
     }
 }
-
