af9e04e0ba1744bca8ea65e7831fea0084c0e5c3
khangkt03 <duykhangkt03@gmail.com>
2023-10-20 11:16:11 +0700

    Connect login to client

--- Client/src/page/auth/Register/Register.tsx
+++ Client/src/page/auth/Register/Register.tsx
@@ -1,14 +1,45 @@
-import { Form } from "react-router-dom";
+import { useEffect } from "react";
+import { Form, useActionData, useNavigate, useSubmit } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Register = () => {
+  const submit = useSubmit();
+
+  const result = useActionData();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (result) {
+      navigate("/")
+    }
+  }, [result]);
+
+  const submitRegister = (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const password = formData.get("password");
+    const repassword = formData.get("repassword");
+    if (repassword !== password) {
+      toast("password and repassword is different", {
+        type: toast.TYPE.WARNING,
+      });
+    } else {
+      submit(formData, {method:"post"});
+    }
+  };
   return (
     <>
       <div className="px-72 mb-20">
-        <div className="mb-20 pt-36 text-4xl text-center font-bold">Register</div>
-        <Form method="post">
+        <div className="mb-20 pt-36 text-4xl text-center font-bold">
+          Register
+        </div>
+        <Form onSubmit={(event) => submitRegister(event)} method="post">
           <div className="mb-3">
             <label className="font-bold">User Name</label>
             <input
+              name="username"
               className="p-2 w-full mb-10 border"
               placeholder="User Name"
               required
@@ -18,6 +49,7 @@ const Register = () => {
           <div className="mb-3">
             <label className="font-bold">Password</label>
             <input
+              name="password"
               type="password"
               className="p-2 w-full mb-10 border"
               placeholder="Password"
@@ -28,6 +60,7 @@ const Register = () => {
           <div className="mb-3">
             <label className="font-bold">Re-Password</label>
             <input
+              name="repassword"
               type="password"
               className="p-2 w-full mb-10 border"
               placeholder="re input your password"
@@ -38,6 +71,7 @@ const Register = () => {
           <div className="mb-3">
             <label className="font-bold">Email</label>
             <input
+              name="email"
               type="email"
               className="p-2 w-full mb-10 border"
               placeholder="Email"
@@ -48,6 +82,7 @@ const Register = () => {
           <div className="mb-3">
             <label className="font-bold">Phone</label>
             <input
+              name="phone"
               type="tel"
               className="p-2 w-full mb-10 border"
               placeholder="Phone Number"
@@ -58,6 +93,7 @@ const Register = () => {
           <div className="mb-3">
             <label className="font-bold">Date of Birth</label>
             <input
+              name="dob"
               type="date"
               className="p-2 w-full mb-10 border"
               placeholder="Date of Birth"
