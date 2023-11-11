81b06f7e66eea4d95e520eddff5ce04366c4829c
khangkt03 <duykhangkt03@gmail.com>
2023-11-11 09:06:04 +0700

    fixbug feedback

--- Client/src/components/CourseShow.tsx
+++ Client/src/components/CourseShow.tsx
@@ -1,9 +1,16 @@
-
 import { Link, useNavigate } from "react-router-dom";
 import { Course } from "../Type/Type";
 import { API } from "../API/API";
+import { useState } from "react";
+import { Box, Modal } from "@mui/material";
+import { modalCOurseStyle } from "../css/modalStyle";
+import ReportPage from "../page/ReportPage";
 
 export const MyCourseShow = ({ course }: { course: Course | any }) => {
+  const [open, setOpen] = useState(false);
+  const [modalCourse, setModalCourse]: any = useState();
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const navigate = useNavigate();
   return (
     <div className="w-3/12 mx-10 border-4 mb-5">
@@ -11,7 +18,10 @@ export const MyCourseShow = ({ course }: { course: Course | any }) => {
         onClick={() => navigate(`/Lessions/${course.courseId}`)}
         className="border-b-4 border-black"
       >
-        <img className="w-4/5 m-auto mt-5 mb-10" src={course.courseImage} />
+        <img
+          className="w-4/5 h-40 m-auto mt-5 mb-10"
+          src={course.courseImage}
+        />
         <div className="flex justify-end">
           <div className="text-center text-xs bg-yellow-500 w-1/4">
             {course?.levelModel !== undefined
@@ -23,11 +33,55 @@ export const MyCourseShow = ({ course }: { course: Course | any }) => {
       <div className="text-center text-3xl font-normal py-3">
         {course?.title}
       </div>
+
+      <div className="text-center my-5">
+        <Link
+          to={`/Lessions/${course.courseId}`}
+          className="p-2 bg-green-700 text-white mt-10 px-5"
+        >
+          Học
+        </Link>
+      </div>
+      {/* <div className="text-center my-5">
+        <button>
+          <button
+            onClick={() => {
+              setModalCourse(course);
+              handleOpen();
+            }}
+            className="p-2 px-6 bg-green-700 text-white"
+          >
+            Feedback
+          </button>
+        </button>
+      </div> */}
+
+      {/* <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={modalCOurseStyle}>
+            <ReportPage
+              courseId={modalCourse?.courseId}
+              handleClose={handleClose}
+            />
+          </Box>
+        </Modal>
+      </div> */}
     </div>
   );
 };
 
-export const InstructorCourseShow = ({ course, getMyCourses }: { course: Course | any, getMyCourses:any }) => {
+export const InstructorCourseShow = ({
+  course,
+  getMyCourses,
+}: {
+  course: Course | any;
+  getMyCourses: any;
+}) => {
   const navigate = useNavigate();
   return (
     <div className="w-3/12 mx-10 border-4 mb-5">
@@ -56,17 +110,28 @@ export const InstructorCourseShow = ({ course, getMyCourses }: { course: Course 
           Edit Course
         </Link>
         <div>
-          <button onClick={async() => {
-            await API.deleteCourse(course.courseId)
-            await getMyCourses()
-          }} className="p-2 px-6 bg-green-700 text-white mt-10">Delete</button>
+          <button
+            onClick={async () => {
+              await API.deleteCourse(course.courseId);
+              await getMyCourses();
+            }}
+            className="p-2 px-6 bg-green-700 text-white mt-10"
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
   );
 };
 
-export const StaffCourseShow = ({ course, getMyCourses }: { course: Course | any, getMyCourses:any }) => {
+export const StaffCourseShow = ({
+  course,
+  getMyCourses,
+}: {
+  course: Course | any;
+  getMyCourses: any;
+}) => {
   const navigate = useNavigate();
   return (
     <div className="w-3/12 mx-10 border-4 mb-5">
@@ -74,7 +139,10 @@ export const StaffCourseShow = ({ course, getMyCourses }: { course: Course | any
         onClick={() => navigate(`/Lessions/${course.courseId}`)}
         className="border-b-4 border-black"
       >
-        <img className="w-4/5 h-40 m-auto mt-5 mb-10" src={course.courseImage} />
+        <img
+          className="w-4/5 h-40 m-auto mt-5 mb-10"
+          src={course.courseImage}
+        />
         <div className="flex justify-end">
           <div className="text-center text-xs bg-yellow-500 w-1/4">
             {course?.levelModel !== undefined
@@ -89,10 +157,15 @@ export const StaffCourseShow = ({ course, getMyCourses }: { course: Course | any
 
       <div className="text-center my-5">
         <div>
-          <button onClick={async() => {
-            await API.setTrueCourse(course.courseId)
-            await getMyCourses()
-          }} className="p-2 px-6 bg-green-700 text-white mt-10">Phê Duyệt</button>
+          <button
+            onClick={async () => {
+              await API.setTrueCourse(course.courseId);
+              await getMyCourses();
+            }}
+            className="p-2 px-6 bg-green-700 text-white mt-10"
+          >
+            Phê Duyệt
+          </button>
         </div>
       </div>
     </div>
@@ -100,8 +173,6 @@ export const StaffCourseShow = ({ course, getMyCourses }: { course: Course | any
 };
 
 export const CourseShow = ({ course }: { course: Course | any }) => {
-  
-
   return (
     <div>
       <div className="border-b-4 border-black">
@@ -117,8 +188,6 @@ export const CourseShow = ({ course }: { course: Course | any }) => {
       <div className="text-center text-3xl font-normal py-3">
         {course?.title}
       </div>
-
-      
     </div>
   );
 };
--- Client/src/page/ReportPage.tsx
+++ Client/src/page/ReportPage.tsx
@@ -1,6 +1,20 @@
 import { Form } from "react-router-dom";
+import { API } from "../API/API";
 
-const ReportPage = () => {
+const ReportPage = ({courseId, handleClose}:{courseId:number, handleClose:any}) => {
+
+  const submitFeedback = async(event:any) => {
+    event.preventDefault();
+    const formData = new FormData(event.target)
+    const data = {
+      comment: formData.get('comment'),
+      course_id: courseId,
+      reviewDate: new Date(),
+    }
+
+    await API.addFeedback(data)
+    handleClose()
+  }
   return (
     
       <div className="pt-32 pl-20">
@@ -11,22 +25,10 @@ const ReportPage = () => {
             src="https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133352156-stock-illustration-default-placeholder-profile-icon.jpg"
           />
         </div>
-        <Form method="post" className="mt-10 mb-36 font-bold">
+        <Form onSubmit={submitFeedback} method="post" className="mt-10 mb-36 font-bold">
           <div className="mb-3">
-            <label>UserName</label>
-            <input className="block p-2 w-full border" />
-          </div>
-          <div className="mb-3">
-            <label>Email</label>
-            <input type="email" className="block p-2 w-full border" />
-          </div>
-          <div className="mb-3">
-            <label>Title</label>
-            <input className="block p-2 w-full border" />
-          </div>
-          <div className="mb-3">
-            <label>Content</label>
-            <textarea className="block p-2 w-full border h-60"></textarea>
+            <label>Feedback</label>
+            <textarea name="comment" required className="block p-2 w-full border h-60"></textarea>
           </div>
           <div className="flex justify-center px-20 mt-10">
             <button className="bg-orange-400 text-white py-2 px-5">
